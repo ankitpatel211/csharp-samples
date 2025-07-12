67a6a466d690511135200cec8e4463f20d9ba456
Ankit Patel <wd.ankit.patel@gmail.com>
2025-07-12 12:57:09 +0530

    optimized coding practice

--- RegularExpression/ConsoleOutput.cs
+++ RegularExpression/ConsoleOutput.cs
@@ -1,28 +1,41 @@
 ﻿namespace RegularExpression
 {
-    public static class ConsoleOutput
-    {
-        public const string dateFormat = "MM/dd/yyyy";
-        public const string timeFormat = "hh:mm tt";
+    public static class ConsoleOutput {
+        public const string inputText = "Input= ";
+        public const string outputText = "Output= ";
+    }
+    public static class TwelveHoursTimeRegEx {
+        public const string expression = "(((0?[1-9]|1[0-2]):([0-5][0-9]))\\s?(am|AM|pm|PM))";
+    }
+    public static class DateTimeFormat {
+        public const string date = "MM/dd/yyyy";
+        public const string time = "hh:mm tt";
+    }
+    public static class TwelveHoursFormat {
         public const string am = "AM";
         public const string pm = "PM";
-        public const string regExp = "(((0?[1-9]|1[0-2]):([0-5][0-9]))\\s?(am|AM|pm|PM))";
+    }
+    public static class ExceptionOutput {
+        public const string falseMatch = "There is no search text matching successfully according to a regular expression.";
+        public const string nonInteger = "Invalid input! cannot convert to integer.";
+        public const string overflowInteger = "Value is too large for a conversion of an integer.";
+    }
+    public static class Split {
         public const string space = " ";
         public const string colon = ":";
-        public const string inputText = "Input = ";
-        public const string and = " and ";
-        public const string outputText = "Output = ";
-        public const string falseMatchOutput = "There is no search text matching successfully according to a regular expression.";
-        public const string nonIntegerOutput = "Invalid input! cannot convert to integer.";
-        public const string overflowIntegerOutput = "Value is too large for a conversion of an integer.";
-        public const string hours = " hours";
-        public const string hour = " hour";
-        public const string minutes = " minutes";
-        public const string minute = " minute";
-        public const string morning = " in the morning";
-        public const string afternoon = " in the afternoon";
-        public const string evening = " in the evening";
-        public const string night = " in the night";
-        public const string midNight = " in the midnight";
+    }
+    public static class DayParts {
+        public const string morning = "in the morning";
+        public const string afternoon = "in the afternoon";
+        public const string evening = "in the evening";
+        public const string night = "in the night";
+        public const string midNight = "in the midnight";
+    }
+    public static class HourMinute {
+        public const string hours = "hours";
+        public const string hour = "hour";
+        public const string and = "and";
+        public const string minutes = "minutes";
+        public const string minute = "minute";
     }
 }
--- RegularExpression/DateTimeToday.cs
+++ RegularExpression/DateTimeToday.cs
@@ -2,50 +2,50 @@
 using System.Text.RegularExpressions;
 using RegularExpression;
 
-string dateToday = DateTime.Now.ToString(ConsoleOutput.dateFormat).Trim();
-string currentTime = DateTime.Now.ToString(ConsoleOutput.timeFormat).Trim();
+string dateToday = DateTime.Now.ToString(DateTimeFormat.date).Trim();
+string currentTime = DateTime.Now.ToString(DateTimeFormat.time).Trim();
 string input = "Today is " + dateToday + " and current time is " + currentTime + ".";
 string inputCmd = string.Concat(ConsoleOutput.inputText, input);
 Console.WriteLine(inputCmd);
-Regex regex = new(ConsoleOutput.regExp);
+Regex regex = new(TwelveHoursTimeRegEx.expression);
 var match = regex.Match(input);
 bool isMatched = match.Success;
 if (isMatched) {
     string matchValue = match.Value;
-    var hoursMinutes = string.IsNullOrEmpty(matchValue) ? currentTime.Split(ConsoleOutput.space) : matchValue.Split(ConsoleOutput.space);
-    string hourText = hoursMinutes[0].Split(ConsoleOutput.colon)[0];
-    string minuteText = hoursMinutes[0].Split(ConsoleOutput.colon)[1];
+    var hoursMinutes = string.IsNullOrEmpty(matchValue) ? currentTime.Split(Split.space) : matchValue.Split(Split.space);
+    string hourText = hoursMinutes[0].Split(Split.colon)[0];
+    string minuteText = hoursMinutes[0].Split(Split.colon)[1];
     string amPm = hoursMinutes[1];
     try {
         int hour = int.Parse(hourText);
         int minute = int.Parse(minuteText);
-        string hoursRange = hour >= 02 && hour <= 12 ? ConsoleOutput.hours : ConsoleOutput.hour;
-        string minutesRange = minute >= 02 && minute <= 59 ? ConsoleOutput.minutes : ConsoleOutput.minute;
-        string hoursTime = string.Concat(hourText, hoursRange);
-        string minutesTime = string.Concat(minuteText, minutesRange);
-        var replaceText = string.Concat(hoursTime, ConsoleOutput.and, minutesTime);
-        if (hour >= 07 && hour < 12 && amPm == ConsoleOutput.am) {
-            replaceText += ConsoleOutput.morning;
-        } else if (hour >= 01 && hour <= 03 || hour == 12 && amPm == ConsoleOutput.pm) {
-            replaceText += ConsoleOutput.afternoon;
-        } else if (hour >= 04 && hour <= 06 && amPm == ConsoleOutput.pm) {
-            replaceText += ConsoleOutput.evening;
-        } else if (hour >= 07 && hour < 12 && amPm == ConsoleOutput.pm) {
-            replaceText += ConsoleOutput.night;
-        } else if (hour >= 01 && hour <= 06 || hour == 12 && amPm == ConsoleOutput.am) {
-            replaceText += ConsoleOutput.midNight;
+        string hoursRange = hour >= 02 && hour <= 12 ? HourMinute.hours : HourMinute.hour;
+        string minutesRange = minute >= 02 && minute <= 59 ? HourMinute.minutes : HourMinute.minute;
+        string hoursTime = string.Concat(hourText, Split.space, hoursRange);
+        string minutesTime = string.Concat(minuteText, Split.space, minutesRange);
+        var replaceText = string.Concat(hoursTime, Split.space, HourMinute.and, Split.space, minutesTime);
+        if (hour >= 07 && hour < 12 && amPm == TwelveHoursFormat.am) {
+            replaceText += Split.space + DayParts.morning;
+        } else if (hour >= 01 && hour <= 03 || hour == 12 && amPm == TwelveHoursFormat.pm) {
+            replaceText += Split.space + DayParts.afternoon;
+        } else if (hour >= 04 && hour <= 06 && amPm == TwelveHoursFormat.pm) {
+            replaceText += Split.space + DayParts.evening;
+        } else if (hour >= 07 && hour < 12 && amPm == TwelveHoursFormat.pm) {
+            replaceText += Split.space + DayParts.night;
+        } else if (hour >= 01 && hour <= 06 || hour == 12 && amPm == TwelveHoursFormat.am) {
+            replaceText += Split.space + DayParts.midNight;
         }
         var output = input.Replace(matchValue, replaceText);
         string outputCmd = string.Concat(ConsoleOutput.outputText, output);
         Console.WriteLine(outputCmd);
     } catch (FormatException) {
-        string outputCmd = string.Concat(ConsoleOutput.outputText, ConsoleOutput.nonIntegerOutput);
+        string outputCmd = string.Concat(ConsoleOutput.outputText, ExceptionOutput.nonInteger);
         Console.WriteLine(outputCmd);
     } catch (OverflowException) {
-        string outputCmd = string.Concat(ConsoleOutput.outputText, ConsoleOutput.overflowIntegerOutput);
+        string outputCmd = string.Concat(ConsoleOutput.outputText, ExceptionOutput.overflowInteger);
         Console.WriteLine(outputCmd);
     }
 } else {
-    string outputCmd = string.Concat(ConsoleOutput.outputText, ConsoleOutput.falseMatchOutput);
+    string outputCmd = string.Concat(ConsoleOutput.outputText, ExceptionOutput.falseMatch);
     Console.WriteLine(outputCmd);
 }

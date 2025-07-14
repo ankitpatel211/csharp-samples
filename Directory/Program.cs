194fc85f0de0980c537805cf53fae7e2ac3adad7
Ankit Patel <wd.ankit.patel@gmail.com>
2025-07-14 09:57:01 +0530

    Filename and Filepath Finder

---
+++ Directory/Program.cs
@@ -0,0 +1,5 @@
+﻿
+
+String directoryPath = "C:/Users/patel/source/repos/ankitpatel211/CSharp-Samples/Directory/Directory.cs";
+foreach (char c in directoryPath)
+   Console.WriteLine(c);
---
+++ DirectoryPath/Program.cs
@@ -0,0 +1,22 @@
+﻿
+string directoryPath = "C:/Users/patel/source/repos/ankitpatel211/CSharp-Samples/Directory/Directory.cs";
+string directory = string.Empty;
+string fileName = string.Empty;
+string reverseFilename = string.Empty;
+int slashIndex = 0;
+
+for (int i = directoryPath.Length - 1; i >= 0; i--) {
+    if (directoryPath[i] == '/') {
+        slashIndex = i;
+        break;
+    }
+    reverseFilename += directoryPath[i];
+}
+for (int j = reverseFilename.Length - 1; j >= 0; j--) {
+    fileName += reverseFilename[j];
+}
+Console.WriteLine("Filename: " + fileName);
+for (int i = 0; i < slashIndex; i++) {
+    directory = directory + directoryPath[i];
+}
+Console.WriteLine("Filepath: " + directory);

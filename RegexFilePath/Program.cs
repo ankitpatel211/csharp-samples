24c4aa7e0f557f874b255fe8d329e6f8f2ad98cb
Ankit Patel <wd.ankit.patel@gmail.com>
2025-07-20 17:13:24 +0530

    Regular expression upgrade

--- RegexFilePath/Program.cs
+++ RegexFilePath/Program.cs
@@ -3,7 +3,7 @@
 using System.Text.RegularExpressions;
 
 string filePath = "C:\\Users\\patel\\source\\repos\\ankitpatel211\\CSharp-Samples\\Directory\\Directory.cs";
-string fileOrFolderRegex = @"[a-zA-Z0-9_-]+(\.[a-zA-Z]{2,4})?\/?\\?$";
+string fileOrFolderRegex = @"[a-zA-Z0-9_-]+(\.[a-zA-Z]{2,4})?\/?\\?\\?$";
 string newFilePath = Regex.Replace(filePath, fileOrFolderRegex, string.Empty);
 string fileName = Regex.Match(filePath, fileOrFolderRegex).Value;
 string folderName = Regex.Match(newFilePath, fileOrFolderRegex).Value;
@@ -12,7 +12,5 @@ Console.WriteLine("FileName= " + fileName);
 Console.WriteLine("FilePath= " + newFilePath);
 Console.WriteLine("FolderName= " + folderName);
 Console.WriteLine("FolderPath= " + folderPath);
-
-
 
 

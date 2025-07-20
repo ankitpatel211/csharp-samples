97f5d4c4c4a6c18d45bed5a6331a8606e0168b90
Ankit Patel <wd.ankit.patel@gmail.com>
2025-07-20 20:54:14 +0530

    Improved version of a Program

--- RegexFilePath/Program.cs
+++ RegexFilePath/Program.cs
@@ -3,14 +3,29 @@
 using System.Text.RegularExpressions;
 
 string filePath = "C:\\Users\\patel\\source\\repos\\ankitpatel211\\CSharp-Samples\\Directory\\Directory.cs";
-string fileOrFolderRegex = @"[a-zA-Z0-9_-]+(\.[a-zA-Z]{2,4})?\/?\/?\\?\\?$";
-string newFilePath = Regex.Replace(filePath, fileOrFolderRegex, string.Empty);
-string fileName = Regex.Match(filePath, fileOrFolderRegex).Value;
-string folderName = Regex.Match(newFilePath, fileOrFolderRegex).Value;
-string folderPath = Regex.Replace(newFilePath, fileOrFolderRegex, string.Empty);
-Console.WriteLine("FileName= " + fileName);
-Console.WriteLine("FilePath= " + newFilePath);
-Console.WriteLine("FolderName= " + folderName);
-Console.WriteLine("FolderPath= " + folderPath);
+string fileName = string.Empty;
+string newFilePath = string.Empty;
+string folderName;
+string folderPath;
+string fileOrFolderRegex = @"[a-zA-Z0-9_\s-]+(\.[a-zA-Z]{2,4})?\/?\/?\\?\\?$";
+string fileNameRegex = @"[a-zA-Z0-9_\s-]+(\.[a-zA-Z]{2,4})";
+bool isFileName = Regex.IsMatch(filePath, fileNameRegex);
+if (isFileName) {
+    fileName = Regex.Match(filePath, fileOrFolderRegex).Value;
+    newFilePath = Regex.Replace(filePath, fileOrFolderRegex, string.Empty);
+    folderName = Regex.Match(newFilePath, fileOrFolderRegex).Value;
+    folderPath = Regex.Replace(newFilePath, fileOrFolderRegex, string.Empty);
+} else {
+    folderName = Regex.Match(filePath, fileOrFolderRegex).Value;
+    folderPath = Regex.Replace(filePath, fileOrFolderRegex, string.Empty);
+}
+if (!string.IsNullOrEmpty(newFilePath))
+    Console.WriteLine("FilePath= " + newFilePath);
+if (!string.IsNullOrEmpty(fileName))
+    Console.WriteLine("FileName= " + fileName);
+if (!string.IsNullOrEmpty(folderPath))
+    Console.WriteLine("FolderPath= " + folderPath);
+if (!string.IsNullOrEmpty(folderName))
+    Console.WriteLine("FolderName= " + folderName);
 
 

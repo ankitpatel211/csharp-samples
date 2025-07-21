507e511249df975f297b25d5d4ed4e021198eea4
Ankit Patel <wd.ankit.patel@gmail.com>
2025-07-21 11:25:59 +0530

    Improved version of this program

--- RegexFilePath/Program.cs
+++ RegexFilePath/Program.cs
@@ -2,30 +2,45 @@
 
 using System.Text.RegularExpressions;
 
-string filePath = "C:\\Users\\patel\\source\\repos\\ankitpatel211\\CSharp-Samples\\Directory\\Directory.cs";
+string path = @"C:\Downloads\SwedenConverse.png";
 string fileName = string.Empty;
-string newFilePath = string.Empty;
-string folderName;
-string folderPath;
+string filePath = string.Empty;
+string folderName = string.Empty;
+string folderPath = string.Empty;
+string drive;
+string driveRegex = @"^[a-zA-Z]:\/?\/?\\?\\?";
+bool isDrive = Regex.IsMatch(path, driveRegex);
+Console.WriteLine("Input= " + path);
+Console.Write("Output= ");
+if (string.IsNullOrEmpty(path) || !isDrive) {
+    Console.WriteLine("Path should be valid like drive:/, drive:/folder/, or drive:/folder/file");
+    return;
+}
 string fileOrFolderRegex = @"[a-zA-Z0-9_\s-]+(\.[a-zA-Z]{2,4})?\/?\/?\\?\\?$";
-string fileNameRegex = @"[a-zA-Z0-9_\s-]+(\.[a-zA-Z]{2,4})";
-bool isFileName = Regex.IsMatch(filePath, fileNameRegex);
+string folderNameRegex = @"[a-zA-Z0-9_\s-]+\/?\/?\\?\\?$";
+string fileNameRegex = @"[a-zA-Z0-9_\s-]+(\.[a-zA-Z]{2,4})$";
+drive = Regex.Match(path, driveRegex).Value;
+bool isFolderName = Regex.IsMatch(path, folderNameRegex);
+bool isFileName = Regex.IsMatch(path, fileNameRegex);
 if (isFileName) {
-    fileName = Regex.Match(filePath, fileOrFolderRegex).Value;
-    newFilePath = Regex.Replace(filePath, fileOrFolderRegex, string.Empty);
-    folderName = Regex.Match(newFilePath, fileOrFolderRegex).Value;
-    folderPath = Regex.Replace(newFilePath, fileOrFolderRegex, string.Empty);
-} else {
+    fileName = Regex.Match(path, fileOrFolderRegex).Value;
+    filePath = Regex.Replace(path, fileOrFolderRegex, string.Empty);
     folderName = Regex.Match(filePath, fileOrFolderRegex).Value;
     folderPath = Regex.Replace(filePath, fileOrFolderRegex, string.Empty);
+} else if (isFolderName) {
+    folderName = Regex.Match(path, fileOrFolderRegex).Value;
+    folderPath = Regex.Replace(path, fileOrFolderRegex, string.Empty);
 }
-if (!string.IsNullOrEmpty(newFilePath))
-    Console.WriteLine("FilePath= " + newFilePath);
-if (!string.IsNullOrEmpty(fileName))
-    Console.WriteLine("FileName= " + fileName);
-if (!string.IsNullOrEmpty(folderPath))
-    Console.WriteLine("FolderPath= " + folderPath);
+if (!string.IsNullOrEmpty(drive))
+    Console.WriteLine("Drive= " + drive);
+if (!string.IsNullOrEmpty(folderName)
+    && !string.IsNullOrEmpty(folderPath))
+    Console.WriteLine("\tFolderPath= " + folderPath);
 if (!string.IsNullOrEmpty(folderName))
-    Console.WriteLine("FolderName= " + folderName);
-
+    Console.WriteLine("\tFolderName= " + folderName);
+if (!string.IsNullOrEmpty(fileName) 
+    && !string.IsNullOrEmpty(filePath))
+    Console.WriteLine("\tFilePath= " + filePath);
+if (!string.IsNullOrEmpty(fileName))
+    Console.WriteLine("\tFileName= " + fileName);
 
